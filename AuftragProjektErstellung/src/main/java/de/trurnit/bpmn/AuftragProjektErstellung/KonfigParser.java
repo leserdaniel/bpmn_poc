ab2cfac21175b5f7092948202967fab4b2b0ed5a
leserdaniel <leser.daniel@trurnit.de>
2017-05-19 17:07:13 +0200

    Gateway und Setzen von Prozessvariablen in Delegate Code

--- AuftragProjektErstellung/src/main/java/de/trurnit/bpmn/AuftragProjektErstellung/KonfigParser.java
+++ AuftragProjektErstellung/src/main/java/de/trurnit/bpmn/AuftragProjektErstellung/KonfigParser.java
@@ -5,19 +5,24 @@ import java.util.logging.Logger;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-/**
- * This is an empty service implementation illustrating how to use a plain Java
- * class as a BPMN 2.0 Service Task delegate.
- */
+public class KonfigParser implements JavaDelegate {
 
-public class Test implements JavaDelegate {
-	
 	private final Logger LOGGER = Logger.getLogger(LoggerDelegate.class.getName());
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
+
 		String konfig_pfad = (String) execution.getVariable("konfig_pfad");
-		LOGGER.info("222XXXXXXXXX die Konfiguration liegt dort: " + konfig_pfad);
+
+		if (konfig_pfad.length() > 5) {
+			execution.setVariable("konfig_check", "passed");
+		} else {
+			execution.setVariable("konfig_check", "failed");
+
+		}
+		String konfig_check = (String) execution.getVariable("konfig_check");
+
+		LOGGER.info("KonfigParser ausgeführt: konfig_pfad " + konfig_pfad + " konfig_check " + konfig_check);
 
 	}
 
